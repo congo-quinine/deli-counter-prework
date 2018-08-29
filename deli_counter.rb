f6963a6cb2c1138afa074ab6cb4f6d7fba44be18
Rob Parker <into.the.parker@gmail.com>
2018-08-29 12:15:21 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,7 +15,7 @@ def line(katz_deli)
                  in_line += " #{counter}. #{i}"
               counter += 1
             end
-  else
+  elsif katz_deli == 0
     puts "The line is currently empty."
   end
   puts in_line
