d89e21c0fc139a5ceef2812ae63562fb049230b6
Rob Parker <into.the.parker@gmail.com>
2018-08-29 12:49:31 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -9,7 +9,7 @@
 
   def line(katz_deli)
     if katz_deli.size == 0
-      puts "The line is currently empty."
+
     else
       counter = 1
           in_line = "The line is currently:"
@@ -17,8 +17,9 @@
                    in_line += " #{counter}. #{i}"
                 counter += 1
               end
+              puts in_line
             end
-    puts in_line
+            puts "The line is currently empty."
   end
 
 # Build a method that a new customer will use when entering the deli. The take_a_number method should accept two arguments, the array for the current line of people (katz_deli), and a string containing the name of the person joining the end of the line. The method should call out (puts) the person's name along with their position in line. Top-Tip: Remember that people like to count from 1, not from 0 ("zero") like computers.
