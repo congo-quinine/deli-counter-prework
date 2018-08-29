b5f603a52f50c39c740157b5c3834000778f7a2b
Rob Parker <into.the.parker@gmail.com>
2018-08-29 14:46:53 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -37,9 +37,9 @@
 
   def now_serving(katz_deli)
     if katz_deli.size == 0
-      puts "The line is empty" 
+      puts "The line is empty"
         else
           puts "Now serving #{katz_deli[0]}"
             katz_deli.shift
-    end  
-  end  
+    end
+  end
