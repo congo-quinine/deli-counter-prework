f2e2282afd1e182ce3c505116e9d2d3d425036f5
Rob Parker <into.the.parker@gmail.com>
2018-08-29 12:40:20 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -28,7 +28,7 @@
     place_in_line = katz_deli.size
     puts "Welcome, #{name}. You are number #{place_in_line } in line."
   elsif katz_deli.size > 0
-    katz_deli.unshift(name)
+    katz_deli.push(name)
     place_in_line = katz_deli.size
     puts "Welcome, #{name}. You are number #{place_in_line + 1} in line."
   end
