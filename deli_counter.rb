d519da921d30b1e494eb3ddaf58098213c2ac320
Rob Parker <into.the.parker@gmail.com>
2018-08-29 12:18:47 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,15 +10,15 @@
 katz_deli = []
 
 def line(katz_deli)
-  if katz_deli.size > 0
+  if katz_deli.size == 0
+    puts "The line is currently empty."
+  else
     counter = 1
         in_line = "The line is currently:"
           katz_deli.each do |i|
                  in_line += " #{counter}. #{i}"
               counter += 1
             end
-  elsif katz_deli.size == 0
-    puts "The line is currently empty."
   end
   puts in_line
 end
